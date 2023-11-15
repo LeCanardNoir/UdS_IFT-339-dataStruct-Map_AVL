dd44eb5ef8f95c54962bbb13a9ae1f2257697be1
Bruno Pouliot <dev.lecanardnoir@gmail.com>
2023-11-15 17:45:04 -0500

    BREAK

--- mapImpl.h
+++ mapImpl.h
@@ -58,7 +58,7 @@ void map<Tclef, Tvaleur>::rotation_gauche_droite(noeud*& p)
 template <typename Tclef, typename Tvaleur>
 void map<Tclef, Tvaleur>::rotation_droite_gauche(noeud*& p)
 {
-    noeud* tmp = p->m_droite;
+    /*noeud* tmp = p->m_droite;
     int ia = tmp->m_indice;
     int ib = p->m_indice;
     int nib = -ia - std::max(0, -ia) - 1 + ib;
@@ -67,7 +67,7 @@ void map<Tclef, Tvaleur>::rotation_droite_gauche(noeud*& p)
     p->m_indice = nib;
     p->m_droite = tmp->m_gauche;
     tmp->m_droite = p;
-    p = tmp;
+    p = tmp;*/
 }
 
 
