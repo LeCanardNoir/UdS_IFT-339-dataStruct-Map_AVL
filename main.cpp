4adb96c71bf404524d43a86b7b7646d6400b563c
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-11-15 16:54:49 -0500

    Progress: MAIN test

--- main.cpp
+++ main.cpp
@@ -8,6 +8,20 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	map<string, string> m1;
+	m1.insert("help", "I'm stuck");
+	m1.insert("Bruno", "Is a fucking dumb");
+	m1.insert("Gab", "Is a genuis");
+	m1.insert("Mario", "It's MARIO");
+	m1.insert("Batman", "I'm Batman,... i guess!!");
+	m1.insert("Robin", "Who????");
+	m1.insert("Flash", "It's so long");
+	m1.insert("SuperMan", "I'm Clark Ken,... the Barbie's boyfriend");
+	m1.insert("ish", "lebidish");
+	m1.insert("ZZZZZ", "Wake up ostie");
+	m1.insert("zzzzz", "baby ZZZZZ");
+	m1.insert("aaa", "3a");
+
+	m1.afficher(cout);
 	return 0;
 }
