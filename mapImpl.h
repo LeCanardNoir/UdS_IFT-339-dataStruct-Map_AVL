28e8eed974c3b35a4ff164577af139508d718f51
2160630 <104075924+2160630@users.noreply.github.com>
2023-11-15 17:40:32 -0500

    FCT (à tester) : rotation gauche droite

--- mapImpl.h
+++ mapImpl.h
@@ -7,8 +7,8 @@
 //  Modifie par : Vincent Ducharme, Hiver 2018
 //
 //  Devoir fait par
-//     Coéquipier 1 :
-//     Coéquipier 2 :
+//     CoÃ©quipier 1 :
+//     CoÃ©quipier 2 :
 
 #ifndef mapImpl_h
 #define mapImpl_h
@@ -52,6 +52,22 @@ typename map<Tclef, Tvaleur>::iterator map<Tclef, Tvaleur>::insert(iterator j, c
 template <typename Tclef, typename Tvaleur>
 void map<Tclef, Tvaleur>::rotation_gauche_droite(noeud*& p)
 {
+    size_t h1 = p->m_gauche->m_gauche->m_indice;
+    size_t h2 = p->m_gauche->m_droite->m_indice;
+    size_t h3 = p->m_droite->m_indice;
+    size_t ia = h1 - h2;
+    size_t ib = (h1 + max(0, -ia) + 1) - h3;
+    size_t nib = -ia - max(0, -ia) - 1 + ib;
+    size_t nia = ia - max(0, -nib) - 1;
+
+    std::swap(p->m_contenu, p->m_gauche->m_contenu);
+    std::swap(p->m_gauche, p->m_droite);
+    std::swap(p->m_gauche->m_parent, p->m_droite->m_gauche->m_parent);
+    std::swap(p->m_gauche, p->m_droite->m_gauche);
+    std::swap(p->m_droite->m_gauche, p->m_droite->m_droite);
+
+    p->m_indice = nia;
+    p->m_droite->m_indice = nib;
 }
 
 //effectuer une rotation simple de la droite vers la gauche
