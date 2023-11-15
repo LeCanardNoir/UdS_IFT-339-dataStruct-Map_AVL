5553eebe5a86bdec00bfb51efb91c28a5da3d74d
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-11-15 16:00:15 -0500

    INIT: project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿// map.cpp : définit le point d'entrée de l'application.
+//
+
+#include "map.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
